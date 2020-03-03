68e01310f020d04543934574aa21dc47b22e59cc
Filip Laskoski <flaskoski@gmail.com>
2020-03-03 16:16:30 +0100

    commit

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,36 +1,56 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
-
+import ContactData from "./ContactData/ContactData";
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1
-    }
+    ingredients: null,
+    price: 0
+    // {
+    //   salad: 1,
+    //   meat: 1,
+    //   cheese: 1,
+    //   bacon: 1
+    // }
   };
-  componentDidMount() {
+  componentWillMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
+    let price = 0;
     for (let param of query.entries()) {
-      ingredients[param[0]] = +param[1];
+      if (param[0] === "price") {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
     }
-    this.setState({ ingredients: ingredients });
+    this.setState({ ingredients: ingredients, totalPrice: price });
   }
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
   };
   checkoutContinuedHandler = () => {
-    this.props.history.replace("/checkout/contract-data");
+    this.props.history.replace("/checkout/contact-data");
   };
   render() {
+    // console.log("DATA", ContactData);
+    console.log(this.state.totalPrice, "PRICE");
     return (
       <div>
         <CheckoutSummary
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + "/contact-data"}
+          render={props => (
+            <ContactData
+              ingredients={this.state.ingredients}
+              price={this.state.totalPrice}
+              {...props}
+            />
+          )}
         />
       </div>
     );
