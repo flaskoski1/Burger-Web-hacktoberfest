01441dba677ac7bd81278b8c33708a942e2974b9
Filip Laskoski <flaskoski@gmail.com>
2020-03-05 16:06:49 +0100

    dynamic contact data and validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,64 +5,146 @@ import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
 
+// class ContactData extends Component {
+//   state = {
+//     // name: "",
+//     // email: "",
+//     // address: {
+//     //   street: "",
+//     //   postalCode: ""
+//     // },
+//     orderForm: {
+//       // customer: {
+//       name: {
+//         elementType: "input",
+//         elementConfig: {
+//           type: "text",
+//           placeholder: "Your Name"
+//         },
+//         value: ""
+//       },
+//       // }"Max Schwarzmuller",
+//       // address: {
+//       street: {
+//         elementType: "input",
+//         elementConfig: {
+//           type: "text",
+//           placeholder: "Street"
+//         },
+//         value: ""
+//       },
+//       // "Teststreet 1",
+//       zipCode: {
+//         elementType: "input",
+//         elementConfig: {
+//           type: "text",
+//           placeholder: "ZIP Code"
+//         },
+//         value: ""
+//       },
+//       // "41351",
+//       country: {
+//         elementType: "input",
+//         elementConfig: {
+//           type: "text",
+//           placeholder: "Country"
+//         },
+//         value: ""
+//       },
+//       //  "Germany",
+//       // },
+//       email: {
+//         elementType: "input",
+//         elementConfig: {
+//           type: "email",
+//           placeholder: "Your E-Mail"
+//         },
+//         value: ""
+//       },
+//       // "test@test.com",
+//       // },
+//       deliveryMethod: {
+//         elementType: "select",
+//         elementConfig: {
+//           options: [
+//             { value: "fastest", displayValue: "Fastest" },
+//             { value: "cheapest", displayValue: "Cheapest" }
+//           ]
+//           // config: {}
+//           // type: "text",
+//           // placeholder: "Your Name"
+//         },
+//         value: ""
+//       }
+//       // "fastest"
+//     },
+//     loading: false
+//   };
 class ContactData extends Component {
   state = {
-    // name: "",
-    // email: "",
-    // address: {
-    //   street: "",
-    //   postalCode: ""
-    // },
     orderForm: {
-      // customer: {
       name: {
         elementType: "input",
         elementConfig: {
           type: "text",
           placeholder: "Your Name"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
-      // }"Max Schwarzmuller",
-      // address: {
       street: {
         elementType: "input",
         elementConfig: {
           type: "text",
           placeholder: "Street"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
-      // "Teststreet 1",
       zipCode: {
         elementType: "input",
         elementConfig: {
           type: "text",
           placeholder: "ZIP Code"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false
       },
-      // "41351",
       country: {
         elementType: "input",
         elementConfig: {
           type: "text",
           placeholder: "Country"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
-      //  "Germany",
-      // },
       email: {
         elementType: "input",
         elementConfig: {
           type: "email",
           placeholder: "Your E-Mail"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false
       },
-      // "test@test.com",
-      // },
       deliveryMethod: {
         elementType: "select",
         elementConfig: {
@@ -70,22 +152,38 @@ class ContactData extends Component {
             { value: "fastest", displayValue: "Fastest" },
             { value: "cheapest", displayValue: "Cheapest" }
           ]
-          // type: "text",
-          // placeholder: "Your Name"
         },
         value: ""
       }
-      // "fastest"
     },
     loading: false
   };
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.minLength && isValid;
+    }
+    return isValid;
+  }
   orderHandler = event => {
     event.preventDefault();
     // console.log(this.props.ingredients);
     this.setState({ loading: true });
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm)
+      formData[formElementIdentifier] = this.state.orderForm[
+        formElementIdentifier
+      ].value;
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      orderData: formData
       // customer: {
       //   name: "Max Schwarzmuller",
       //   address: {
@@ -105,6 +203,37 @@ class ContactData extends Component {
       })
       .catch(error => this.setState({ loading: false }));
   };
+  inputChangedHandler = (event, inputIdentifier) => {
+    // console.log(event.target.value);
+
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+    updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    console.log(updatedFormElement, "updated");
+    this.setState({ orderForm: updatedOrderForm });
+    // console.log(event.target.value, "value");
+    // console.log(updatedOrderForm, "update");
+  };
+  // inputChangedHandler = (event, inputIdentifier) => {
+  //   const updatedOrderForm = {
+  //     ...this.state.orderForm
+  //   };
+  //   const updatedFormElement = {
+  //     ...updatedOrderForm[inputIdentifier]
+  //   };
+  //   updatedFormElement.value = event.target.value;
+  //   updatedOrderForm[inputIdentifier] = updatedFormElement;
+  //   this.setState({ orderForm: updatedOrderForm });
+  // };
   render() {
     const formElementsArray = [];
     for (let key in this.state.orderForm) {
@@ -114,9 +243,9 @@ class ContactData extends Component {
       });
     }
     // console.log(this.props.price, "price");
-    console.log(formElementsArray, "element");
+    // console.log(formElementsArray, "element");
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {/* <Input
           // inputtype="input"
           // type="text"
@@ -132,6 +261,7 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            changed={event => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
 
@@ -153,9 +283,7 @@ class ContactData extends Component {
           name="postal"
           placeholder="Postal Code"
         ></Input> */}
-        <Button btnType="Success" clicked={this.orderHandler}>
-          ORDER
-        </Button>
+        <Button btnType="Success">ORDER</Button>
       </form>
     );
     if (this.state.loading) {
