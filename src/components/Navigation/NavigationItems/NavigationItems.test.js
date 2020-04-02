14a068e858e0e326f4e54fe29f0d5c216aaab716
flaskoski1 <flaskoski@gmail.com>
2020-04-02 12:45:48 +0200

    testing

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Jest from "jest";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import NavigationItems from "./NavigationItems";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+configure({ adapter: new Adapter() });
+let wrapper;
+beforeEach(() => {
+  const wrapper = shallow(<NavigationItems />);
+});
+describe("<NavigationItems/>", () => {
+  it("should render two <NavigationItem/> elements if not authenticated", () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+  it("should render three <NavigationItem/> elements if  authenticated", () => {
+    const wrapper = shallow(<NavigationItems isAuthenticated />);
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+});
